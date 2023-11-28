ab8acd29753b23e00a169aebd5a8ad0d29d1067b
60280 <zxy203203@ncarzone.com>
2023-11-28 10:55:45 +0800

    modify default route

--- assets/MyView-_YhbFzBf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as n}from"./index-U2joUIAM.js";const o={name:"My",data(){return{}}};function r(a,c,s,_,p,u){return t(),n("div",null,"张三")}const f=e(o,[["render",r]]);export{f as default};
---
+++ assets/MyView-e7ZspaDW.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as n}from"./index-puH57dLB.js";const o={name:"My",data(){return{}}};function r(a,c,s,_,p,u){return t(),n("div",null,"张三")}const f=e(o,[["render",r]]);export{f as default};
