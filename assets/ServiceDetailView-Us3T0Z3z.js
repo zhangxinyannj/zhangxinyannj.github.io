4d5cd295345fca166e5ad5d6cc09d2b117862411
60280 <zxy203203@ncarzone.com>
2023-11-29 10:17:27 +0800

    modify pic

--- assets/ServiceDetailView-Us3T0Z3z.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as s}from"./index-7bO0ygn4.js";const a={name:"ServiceDetail",data(){return{}}},c="/assets/order-detail-QX870ku1.png",o={class:"detail",src:c};function r(n,_,i,d,p,l){return t(),s("img",o)}const f=e(a,[["render",r],["__scopeId","data-v-97c4e5ed"]]);export{f as default};
